ea245ccb4711a9d70882d4f2ef447454790480c7
ssboroo <89896772+ssboroo@users.noreply.github.com>
2026-09-13 21:30:53 +0800

    Fix light and dark theme provider

--- src/components/site/theme-provider.tsx
+++ src/components/site/theme-provider.tsx
@@ -1,5 +1,19 @@
 'use client'
+
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return <NextThemesProvider attribute="class" defaultTheme="system" enableSystem storageKey="socialtool-theme" disableTransitionOnChange>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      enableColorScheme
+      themes={['light', 'dark']}
+      storageKey="socialtool-theme"
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
